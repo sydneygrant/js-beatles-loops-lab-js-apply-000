0e1147a166fe4d86aa0f42830395c3fd11f99456
sydneygrant <sgrant@nyu.edu>
2018-08-15 15:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+  for (var i = 0; i < musicians.length; i++){
+    newArr[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return newArr;
+}
+
+// function johnLennonFacts(facts){
+//   var arr = [];
+//   var count = 0;
+//   var i = 0;
+//   while (count < facts){
+//     arr[i] =facts[count] + "!!!");
+//     count++;
+//   }
+//   return arr;
+// }
+//
+function iLoveTheBeatles(number){
+  var arr = [];
+  var count = 0;
+  do{
+    arr[count] = "I love the Beatles";
+    count++;
+
+  } while (number < 15);
+  return arr;
+}
