74a212e19cec37c3ad94cfebf8af5ef36a89cecc
sydneygrant <sgrant@nyu.edu>
2018-08-15 15:30:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function johnLennonFacts(facts){
 var i=0;
 var newArray=[];
 while (i < facts.length){
-newArray.push(`${facts[i]}` + “!!!”);
+newArray.push(`${facts[i]}` + "!!!");
 i++;
 }
 return newArray;
